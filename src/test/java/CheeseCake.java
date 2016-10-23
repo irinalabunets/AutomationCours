c36cebe81c47481b154539a5c4a3cafdb84d960b
Irina Labunets <iryna659@gmail.com>
2016-10-23 20:11:50 +0300

    import libraries

--- src/test/java/CheeseCake.java
+++ src/test/java/CheeseCake.java
@@ -1,3 +1,10 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class CheeseCake {
     @Test
