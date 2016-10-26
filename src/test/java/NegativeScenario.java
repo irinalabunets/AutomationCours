2ca4d01c5e278767794133e4929f9e1790053251
irinalabunets <iryna659@gmail.com>
2016-10-26 16:16:11 +0300

    implemented the first negative scenario fo linkedin suite

--- src/test/java/NegativeScenario.java
+++ src/test/java/NegativeScenario.java
@@ -1,5 +1,17 @@
-/**
- * Created by user on 026 26.10.16.
- */
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
 public class NegativeScenario {
+    @Test
+    public void negativeLinkedin() throws InterruptedException {
+        System.setProperty("webdriver.gecko.driver", "C:/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://www.linkedin.com/");
+        driver.findElement(By.id("regForm"));
+        driver.findElement(By.id("registration-submit")).click();
+        driver.findElement(By.xpath("//div[@class='reg-alert']/span[contains(text(),'Укажите имя')]"));
+    }
 }
