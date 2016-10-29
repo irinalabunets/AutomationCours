15a70ac6e86ce90da59936d8f89b9650103be375
Iryna Labunets <iryna659@gmail.com>
2016-10-29 11:10:33 +0300

    add webelenents

--- src/test/java/RegistrationPage.java
+++ src/test/java/RegistrationPage.java
@@ -1,3 +1,15 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class RegistrationPage {
+
+    //System.setProperty("webdriver.gecko.driver", "C:/geckodriver.exe");
+    WebDriver driver = new FirefoxDriver();
+
+    private WebElement firstName = driver.findElement(By.id("reg-firstname"));
+    private WebElement lastName = driver.findElement(By.id("reg-lastname"));
+    private WebElement email = driver.findElement(By.id("reg-email"));
+    private WebElement password = driver.findElement(By.id("reg-password"));
 }
